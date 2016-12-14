c06bf8a2f9edcc76fc4ec3f3e4a5e9fff3ccbc0b
Kinasakal <ozan.kinasakal@istpmq-nmxp021.emea.corp.ipgnetwork.com>
2016-12-14 10:38:53 +0300

    Code is documented now

--- script.rb
+++ script.rb
@@ -3,6 +3,7 @@ class Calculation
     @input = input
   end
 
+  #Calculates the time to run the calculate_for_input method
   def get_result
       start_time = Time.now
       result = calculate_for_input
@@ -11,6 +12,7 @@ class Calculation
       puts "Calculated the factorial in #{end_time - start_time} seconds"
   end
 
+  #Calculates the factorial of input
   def calculate_for_input
     total = 1
     @input.downto(1) { |n| total *= n }
@@ -21,8 +23,11 @@ end
 
 puts "Please write an Integer"
 @input = Integer(gets) rescue nil
+
+#Insists to user to write an integer input
 while @input == nil do
   puts "This is not an Integer, please write an Integer"
   @input = Integer(gets) rescue nil
 end
+
 Calculation.new(@input).get_result
